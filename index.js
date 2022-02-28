34490ec0e43efe94d2b843a62297b983f091c9f7
ryan0000 <rlibed@esri.com>
2022-02-28 14:56:51 -0800

    clean up

--- index.js
+++ index.js
@@ -1,55 +1,65 @@
 require([
-  "esri/views/MapView",
   "esri/WebMap",
+  "esri/views/MapView",
+  "esri/widgets/Home",
+  "esri/widgets/Legend",
   "esri/widgets/Search",
   "esri/widgets/Search/LayerSearchSource",
-  "esri/core/watchUtils",
-  "esri/widgets/Legend",
   "esri/widgets/Expand",
-  "esri/widgets/Home",
-  "esri/layers/support/FeatureFilter"
+  "esri/layers/support/FeatureFilter",
+  "esri/core/watchUtils"
 ], (
-  MapView,
   WebMap,
+  MapView,
+  Home,
+  Legend,
   Search,
   LayerSearchSource,
-  watchUtils,
-  Legend,
   Expand,
-  Home,
-  FeatureFilter
+  FeatureFilter,
+  watchUtils
 ) => {
+  // CREATE WEB MAP AND MAP VIEW
   const map = new WebMap({
     portalItem: {
       id: "ddf28dc057b8400dbfa148ef403f7c57"
     }
   });
+
   const view = new MapView({
     container: "viewDiv",
     map
   });
+
   watchUtils.on(view, "map.layers", "change", async () => {
     const layer = view.map.layers.getItemAt(0);
+
+    // CREATE SEARCH SOURCE
 
     const layerSearchSource = new LayerSearchSource({
       layer,
       placeholder: "Search for a country name...",
-      displayField: "name",
+      displayField: "fuel1",
       searchFields: ["country", "country_long"],
       name: layer.title,
       maxResults: 6,
       maxSuggestions: 6
     });
+
+    // ADD WIDGETS I.E. SEARCH, LEGEND, HOME.
+
     const searchWidget = new Search({
       view,
       sources: [layerSearchSource],
       includeDefaultSources: false
     });
+
     const search = new Expand({
       view,
       content: searchWidget,
       expanded: true
     });
+
     const legendWidget = new Legend({
       view
     });
@@ -64,48 +74,44 @@ require([
       view
     });
 
+    view.ui.add(search, "top-right");
+    view.ui.add(legend, "bottom-left");
+    view.ui.add(home, "top-left");
+
+    // CREATE FILTER LIST BASED OFF OF LEGEND WIDGET'S ACTIVE LAYER INFOS
+
     const fLayerView = await view.whenLayerView(layer);
 
     watchUtils.when(legendWidget, "viewModel.activeLayerInfos.length", () => {
       const activeLayerInfo = legendWidget.activeLayerInfos.getItemAt(0);
       watchUtils.when(activeLayerInfo, "legendElements.length", () => {
         const legendInfos = activeLayerInfo.legendElements[0].infos;
-        const pickListNode = document.querySelector("calcite-pick-list");
-
         legendInfos.forEach(info => {
           const pickListItem = document.createElement("calcite-pick-list-item");
           pickListItem.label = info.label;
           pickListItem.value = info.value;
           pickListNode.appendChild(pickListItem);
         });
-
-        pickListNode.addEventListener("calciteListChange", e => {
-          const detail = e.detail;
-          const keys = Array.from(detail.keys());
-
-          const where = keys.map(key => `fuel1 = '${key}'`).join(" OR ");
-
-          if (fLayerView.filter) {
-            fLayerView.filter.where = where;
-          } else {
-            fLayerView.filter = new FeatureFilter({
-              where
-            });
-          }
-
-          if (layerSearchSource.filter) {
-            layerSearchSource.filter.where = where;
-          } else {
-            layerSearchSource.filter = new FeatureFilter({
-              where
-            });
-          }
-        });
       });
     });
 
-    view.ui.add(search, "top-right");
-    view.ui.add(legend, "bottom-left");
-    view.ui.add(home, "top-left");
+    // DYNAMICALLY APPLY FILTERS TO FEATURE LAYER VIEW AND SEARCH WIDGET SEARCH SOURCE
+
+    const pickListNode = document.querySelector("calcite-pick-list");
+    pickListNode.addEventListener("calciteListChange", e => {
+      const detail = e.detail;
+      const keys = Array.from(detail.keys());
+      const where = keys.map(key => `fuel1 = '${key}'`).join(" OR ");
+      if (fLayerView.filter && layerSearchSource.filter) {
+        fLayerView.filter.where = where;
+        layerSearchSource.filter.where = where;
+      } else {
+        const filter = new FeatureFilter({
+          where
+        });
+        fLayerView.filter = filter;
+        layerSearchSource.filter = filter;
+      }
+    });
   });
 });
